aa2cd9eb08ca05ac7ec33937ae14a2ff1c1ebb97
ashbur <18829526908@163.com>
2024-06-12 19:49:11 +0800

    finish common code

--- app/dashboard/page.jsx
+++ app/dashboard/page.jsx
@@ -1,6 +1,7 @@
 import { UserButton } from '@clerk/nextjs'
 import React from 'react'
 import AddNewInterview from './_components/AddNewInterview'
+import InterviewList from './_components/InterviewList'
 
 function Dashboard() {
     return (
@@ -11,6 +12,7 @@ function Dashboard() {
             <div className='grid grid-cols-1 md:grid-cols-3 my-5'>
                 <AddNewInterview />
             </div>
+            <InterviewList />
         </div>
     )
 }
